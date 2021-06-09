07d37d68d74240be0cb95ea219ffee45086e6350
srivaniKammri <srivani.kss@gmail.com>
2021-06-09 19:26:23 +0530

    First commit

---
+++ src/main/java/com/bookzy/bookzybookstore/BookzyBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.bookzy.bookzybookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookzy/bookzybookstore/BookzyBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookzy.bookzybookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
