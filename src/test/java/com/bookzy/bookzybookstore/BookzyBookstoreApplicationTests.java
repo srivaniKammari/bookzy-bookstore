5716b27196c23594617dd7eada2a8466d570b044
srivaniKammari <69762695+srivaniKammari@users.noreply.github.com>
2021-06-25 20:35:00 +0530

    Update BookzyBookstoreApplicationTests.java

--- src/test/java/com/bookzy/bookzybookstore/BookzyBookstoreApplicationTests.java
+++ src/test/java/com/bookzy/bookzybookstore/BookzyBookstoreApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BookzyBookstoreApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    public void dummyTest() {
+        assertTrue(true);
+    }
 
 }
