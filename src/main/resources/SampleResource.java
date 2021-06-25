1f226673dbb15cfbee7baed16070633cc42f6284
srivaniKammari <69762695+srivaniKammari@users.noreply.github.com>
2021-06-25 20:42:30 +0530

    Update and rename application.properties to SampleResource.java

---
+++ src/main/resources/SampleResource.java
@@ -0,0 +1,15 @@
+package de.vorb.wildfly_springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleResource {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello bookzy!";
+    }
+
+}
+
